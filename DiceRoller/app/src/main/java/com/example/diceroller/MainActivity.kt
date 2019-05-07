b6726ca1ab4b4bcd1c32cd7fdacda31a8b15ca6a
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-05-08 03:35:49 +0530

    Add TextView and Button code

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        var resultText: TextView = findViewById(R.id.result_text)
+
+        rollButton.setOnClickListener{ resultText.text = "65" }
+
     }
 }
