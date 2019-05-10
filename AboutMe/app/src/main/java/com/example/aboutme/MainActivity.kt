ce65d2cf58fb1d2213e09dced7d707d1323bee76
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-05-10 19:11:37 +0530

    Add dataBinding Views

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var doneButton: Button
+        lateinit var nicknameTextView: TextView
+        lateinit var nicknameEditText: EditText
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doneButton = findViewById(R.id.done_button)
+        nicknameTextView = findViewById(R.id.nickname_text)
+        nicknameEditText = findViewById(R.id.nickname_edit)
+
+        doneButton.setOnClickListener { displayNickname(it) }
+    }
+
+    private fun displayNickname(view: View) {
+        val nickname: String = nicknameEditText.text.toString()
+        nicknameEditText.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        nicknameTextView.text = nickname
+
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
