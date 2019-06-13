01f4b37e1e6102d9c256bccd51b13d0f0e92c40b
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-06-13 18:01:21 +0000

    Add Timer onStart and onStop

--- DessetPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ DessetPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.dessertpusher
 
 import android.content.ActivityNotFoundException
@@ -33,6 +17,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -77,6 +62,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer()
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -155,6 +141,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("Activity Started")
+        dessertTimer.startTimer()
     }
 
     override fun onResume() {
@@ -172,6 +159,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStop() {
         super.onStop()
         Timber.i("Activity Stopped")
+        dessertTimer.stopTimer()
     }
 
     override fun onDestroy() {
