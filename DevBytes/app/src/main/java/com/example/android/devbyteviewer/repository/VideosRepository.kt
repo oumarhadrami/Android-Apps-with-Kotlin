b6ca636f70d29daab2f2d6b74f79de65009e6219
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-01 20:54:10 +0000

    Fetch Data From repository and clean it

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -12,13 +12,24 @@ import kotlinx.coroutines.withContext
 
 class VideosRepository(private val database: VideosDatabase) {
 
-    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) {
-        it.asDomainModel()
-    }
+    /**
+     * A playlist of videos that can be shown on the screen.
+     */
+    val videos: LiveData<List<Video>> =
+            Transformations.map(database.videoDao.getVideos()) {
+                it.asDomainModel()
+            }
 
+    /**
+     * Refresh the videos stored in the offline cache.
+     *
+     * This function uses the IO dispatcher to ensure the database insert database operation
+     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+     * function is now safe to call from any thread including the Main thread.
+     *
+     * To actually load the videos for use, observe [videos]
+     */
     suspend fun refreshVideos() {
-        val playlist = Network.devbytes.getPlaylist().await()
-        database.videoDao.insertAll(*playlist.asDatabaseModel())
         withContext(Dispatchers.IO) {
             val playlist = Network.devbytes.getPlaylist().await()
             database.videoDao.insertAll(*playlist.asDatabaseModel())
