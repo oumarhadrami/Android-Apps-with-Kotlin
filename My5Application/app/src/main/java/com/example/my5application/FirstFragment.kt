c1c1ae48843676f50cf3f85db737444995f19236
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-08 17:56:06 +0000

    Add navigation from 1st to 2nd fragment with safeargs

--- My5Application/app/src/main/java/com/example/my5application/FirstFragment.kt
+++ My5Application/app/src/main/java/com/example/my5application/FirstFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.my5application.databinding.FragmentFirstBinding
 
 
@@ -16,6 +17,9 @@ class FirstFragment : Fragment() {
 
         val binding : FragmentFirstBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_first, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
         return binding.root
 
     }
