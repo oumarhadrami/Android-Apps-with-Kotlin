fd94b05c64ee2df5355939739587aaca005b08b9
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-09 18:00:17 +0000

    Add UpButton

--- My5Application/app/src/main/java/com/example/my5application/MainActivity.kt
+++ My5Application/app/src/main/java/com/example/my5application/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.my5application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.my5application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
