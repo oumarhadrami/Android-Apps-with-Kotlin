a2f021286242df0adef75a63701786a26d6540a1
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-11 18:16:25 +0000

    Linked first fragment to firstViewModel

--- MyApplication6/app/src/main/java/com/example/myapplication6/firstfragment/FirstViewModel.kt
+++ MyApplication6/app/src/main/java/com/example/myapplication6/firstfragment/FirstViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication6.firstfragment
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class FirstViewModel : ViewModel() {
+
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+    get() = _score
 
 
     override fun onCleared() {
         super.onCleared()
 
+    }
+
+    fun updateScore(){
+        _score.value = (_score.value)?.plus(1)
     }
 }
