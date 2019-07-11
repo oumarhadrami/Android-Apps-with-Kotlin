021f4882d52b3548a2df67e6d0ef00652d1a50e1
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-11 18:50:13 +0000

    Add Transformations.map to make a score into a string

--- MyApplication6/app/src/main/java/com/example/myapplication6/firstfragment/FirstViewModel.kt
+++ MyApplication6/app/src/main/java/com/example/myapplication6/firstfragment/FirstViewModel.kt
@@ -3,6 +3,7 @@ package com.example.myapplication6.firstfragment
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class FirstViewModel : ViewModel() {
@@ -10,6 +11,10 @@ class FirstViewModel : ViewModel() {
     private var _score = MutableLiveData<Int>()
     val score : LiveData<Int>
     get() = _score
+    //Add MapTransformation to livedata score to make it string
+    val scoreString = Transformations.map(score, {score ->
+        score.toString()
+    })
 
     init {
         _score.value = 0
