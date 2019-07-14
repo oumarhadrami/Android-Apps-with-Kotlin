be08b481ecc306bca4088318727ad0bfa1c3d011
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-14 11:47:58 +0000

    Finish setting up databinding

--- My7ApplicationRoom/app/src/main/java/com/example/my7applicationroom/MainActivity.kt
+++ My7ApplicationRoom/app/src/main/java/com/example/my7applicationroom/MainActivity.kt
@@ -5,14 +5,17 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.my7applicationroom.databinding.ActivityMainBinding
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(toolbar)
 
 
