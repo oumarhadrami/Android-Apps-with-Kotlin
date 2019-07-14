e810b12fe4621cf7b9f8782f4c32815af406a242
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-14 12:05:56 +0000

    Add table for characters

--- My7ApplicationRoom/app/src/main/java/com/example/my7applicationroom/database/OnePiece.kt
+++ My7ApplicationRoom/app/src/main/java/com/example/my7applicationroom/database/OnePiece.kt
@@ -1,7 +1,10 @@
 package com.example.my7applicationroom.database
 
+import androidx.room.Entity
 
+@Entity(tableName = "one_piece_characters")
 data class OnePiece(
     var characterId : Long = 0L,
-    var characterName : String = ""
+    var characterName : String = "",
+    var devilFruitType : String = ""
 )
