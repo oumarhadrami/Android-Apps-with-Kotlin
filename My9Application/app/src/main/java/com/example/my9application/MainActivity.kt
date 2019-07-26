20baee6471cdfbafce472b8cd695e6096f2ca5ca
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-26 11:27:28 +0000

    Add ViewPager and TabLayout

--- My9Application/app/src/main/java/com/example/my9application/MainActivity.kt
+++ My9Application/app/src/main/java/com/example/my9application/MainActivity.kt
@@ -3,16 +3,68 @@ package com.example.my9application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.*
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.my9application.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+private const val NUM_PAGES = 2
+
+class MainActivity : FragmentActivity() {
 
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var mPager: ViewPager
+    private lateinit var tabLayout : TabLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        mPager = binding.viewpager
+        tabLayout = binding.tabLayout
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+
+        tabLayout.setupWithViewPager(viewpager)
+
+    }
+
+
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getCount(): Int = NUM_PAGES
+
+        override fun getItem(position: Int): Fragment {
+            return when(position){
+                0 -> FirstFragment()
+                else -> SecondFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position){
+                0 -> "First"
+                else -> "Second"
+            }
+        }
     }
 }
+
