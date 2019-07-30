5d2a2e1660736d251ce964870ebe079be91edc20
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-30 10:53:54 +0000

    Add navigation for activities

--- NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/Main2Activity.kt
+++ NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/Main2Activity.kt
@@ -2,11 +2,17 @@ package com.example.navigationcodelab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navigationcodelab.databinding.ActivityMain2Binding
+import com.example.navigationcodelab.databinding.ActivityMainBinding
 
 class Main2Activity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMain2Binding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main2)
     }
 }
