b2a7aa6036e66f74e4d4c7280565a8b0e5adba69
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-30 12:09:33 +0000

    Add animations for navigation

--- NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/HomeFragment.kt
+++ NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.navOptions
 import com.example.navigationcodelab.databinding.FragmentHomeBinding
 import com.example.navigationcodelab.databinding.FragmentStepTwoBinding
 
@@ -21,6 +22,19 @@ class HomeFragment : Fragment() {
 
         binding.textView.setOnClickListener{
            it.findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToMain2Activity())
+        }
+
+
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        binding.navToDest.setOnClickListener {
+            it.findNavController().navigate(R.id.stepOneFragment, null, options)
         }
 
 
