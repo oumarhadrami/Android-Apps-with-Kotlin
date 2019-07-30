358d99cf20a1fed21678977152c14ba181ad3e7d
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-30 11:38:30 +0000

    Add navigation from fragment to activity

--- NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/MainActivity.kt
+++ NavigationCodeLab/app/src/main/java/com/example/navigationcodelab/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.navigationcodelab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.findNavController
 import com.example.navigationcodelab.databinding.ActivityMainBinding
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-
+        binding.navToMain2.setOnClickListener {
+            startActivity(Intent(this, Main2Activity::class.java))
+        }
     }
 }
