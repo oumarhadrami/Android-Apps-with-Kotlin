ec9d31264f00ca0810a4342a9fa19ca9ca5b6d49
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-07-31 10:42:31 +0000

    Create the goldenJojo Database

---
+++ RoomCodeLab/app/src/main/java/com/example/roomcodelab/database/GoldenJojoDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.roomcodelab.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [GoldenJojo::class], version = 1, exportSchema = false)
+abstract class GoldenJojoDatabase : RoomDatabase(){
+
+    abstract val goldenJojoDao : GoldenJojoDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE : GoldenJojoDatabase? = null
+
+        fun getDatabase(context: Context) : GoldenJojoDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext,
+                    GoldenJojoDatabase::class.java,
+                    "golden_jojos_database")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
